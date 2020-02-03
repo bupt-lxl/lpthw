0f04d8059fc4d26854b23eb0a8f8fe5f7dfca8ca
bupt-lxl <liuxianglis@163.com>
2020-02-03 17:10:49 +0800

    exe1 and exe2

---
+++ exe1.py
@@ -0,0 +1,6 @@
+# print方法
+print("Hello World")
+print("Helllo Again")
+print("I like typing this.")
+print("This is fun!")
+print('I "said" do not touch this.')
---
+++ exe2.py
@@ -0,0 +1,7 @@
+# python中使用'#'进行注释
+print("Hello World")
+print("Helllo Again")
+# 这是一行注释
+print("I like typing this.")
+print("This is fun!") # 这是一行注释
+print('I "said" do not touch this.')
