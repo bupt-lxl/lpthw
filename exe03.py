30fe06f358cc4b60303514eda87297ddc21c0a66
bupt-lxl <liuxianglis@163.com>
2020-02-03 17:36:55 +0800

    exe3 and exe4

---
+++ exe03.py
@@ -0,0 +1,5 @@
+# Numbers and Math
+# +  -  *  /  %  >  <  >=  <=
+print("Hens: ", 20 + 35 / 6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Is it true that 3 + 2 < 5 - 7? ", 3 + 2 < 5 - 7)
---
+++ exe04.py
@@ -0,0 +1,13 @@
+# 变量及命名
+a = 'abc'
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+
+cars_not_driven = cars - drivers
+
+print('There are', cars, "cars")
+print('There are only', drivers, "drivers")
+print("There will be", cars_not_driven, "empty cars today.")
+print(a)
